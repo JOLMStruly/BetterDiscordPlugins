ba4ca80437f09200cc6fcff6b6e9dfb3b05b7717
JOLMStruly <daveyjolms@protonmail.com>
2024-05-06 03:13:29 +0300

    Update MemeSoundsJOLMS.index.js

--- MemeSounds/MemeSoundsJOLMS.index.js
+++ MemeSounds/MemeSoundsJOLMS.index.js
@@ -2,7 +2,7 @@ module.exports = (Plugin, Library) => {
 
     const {Logger, Patcher, Settings} = Library;
     
-    return class MemeSounds extends Plugin {
+    return class MemeSoundsJOLMS extends Plugin {
         onStart() {
             Patcher.before(Logger, "log", (t, a) => {
                 a[0] = "Patched Message: " + a[0];
