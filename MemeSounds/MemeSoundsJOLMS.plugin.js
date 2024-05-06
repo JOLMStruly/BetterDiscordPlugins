ee5e458a1558f15a9091a249f2afd753ebf9ac65
JOLMStruly <daveyjolms@protonmail.com>
2024-05-06 03:31:19 +0300

    Update MemeSoundsJOLMS.plugin.js

--- MemeSounds/MemeSoundsJOLMS.plugin.js
+++ MemeSounds/MemeSoundsJOLMS.plugin.js
@@ -10,7 +10,7 @@ module.exports = (() => {
 			version: "1.0.0",
 			description: "Plays Memetastic sounds when certain words are sent in chat. This plugin was heavily inspired by Metalloriff's bruh plugin so please go check him out!",
 			github: "https://github.com/JOLMStruly/BetterDiscordPlugins/tree/main/MemeSounds/MemeSounds.plugin.js",
-			github_raw: "https://raw.githubusercontent.com/Lonk12/BetterDiscordPlugins/main/MemeSounds/MemeSounds.plugin.js"
+			github_raw: "https://raw.githubusercontent.com/JOLMStruly/BetterDiscordPlugins/tree/main/MemeSounds/MemeSoundsJOLMS.plugin.js"
 		},
 		/* Settings */
 		defaultConfig: [{
